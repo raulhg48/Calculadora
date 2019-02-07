188d8c3d34e81e2445f19bfb5c860c8ee369f1dc
U_35007842 <U_35007842@DESKTOP-CVF38QD>
2019-02-07 08:16:16 +0000

    paso 3 y 4

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -6,6 +6,48 @@
 package calculadora;
 
 public class Calculadora {
+
+    /**
+     * @return the operador1
+     */
+    public int getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public int getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operador
+     */
+    public int getOperador() {
+        return operador;
+    }
+
+    /**
+     * @param operador the operador to set
+     */
+    public void setOperador(int operador) {
+        this.operador = operador;
+    }
     private int operador1;
     private int operador2;
     private int operador;
